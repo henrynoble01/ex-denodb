718f197513b5b5a9e28f30283b46ff2e38f5c141
henrynoble01 <henrynoble01@gmail.com>
2022-05-09 13:25:51 +0100

    chore: init commit

---
+++ deps.ts
@@ -0,0 +1,6 @@
+export {
+  Database,
+  SQLite3Connector,
+  Model,
+  DataTypes,
+} from "https://deno.land/x/denodb@v1.0.40/mod.ts";
---
+++ mod.ts
@@ -0,0 +1,31 @@
+import { Database, SQLite3Connector, Model, DataTypes } from "./deps.ts";
+
+const connector = new SQLite3Connector({
+  filepath: "./database.sqlite",
+});
+
+const db = new Database(connector);
+
+class Business extends Model {
+  // For the table name
+  static table = "businesses";
+  // For TIme_stamps
+  static timestamps = true;
+  // Defining Fields
+  static fields = {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      length: 25,
+    },
+    // Using a Shortcut (check Field Descriptors s to learn more)
+    // name:DataTypes.string(25),
+  };
+  // For Adding Default Values
+  static defaults = {
+    name: "Something about us",
+  };
+}
